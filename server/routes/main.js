1fec1e99f3417c1a79101b679059ef2c7d5c57cd
Erick <erickkan2004@gmail.com>
2025-01-23 21:19:32 -0300

    showing the datas on the blog page

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,13 +7,21 @@ const Post = require('../models/Post');
  * HOME
  */
 
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
     const locals = {
         title: "NodeJs Blog",
         description: "Simple Blog created with NodeJs, Express & MongoDb."
-    };
+    }
 
-    res.render('index', { locals });
+    try {
+        const data = await Post.find();
+        res.render('index', { locals, data });
+
+    } catch (error){
+        console.log(error)
+    }
+
+
 });
 
 
